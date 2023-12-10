def6d44d7a7f94b054027492389c4efd628ba915
yaamai <ya_amai@yahoo.co.jp>
2023-12-10 18:31:47 +0900

    port offset, set function to javascript

--- cmd/oapi-tmpl/main.go
+++ cmd/oapi-tmpl/main.go
@@ -22,76 +22,61 @@ func loadFormatFile(formatPath string) (*excelize.File, error) {
 	}
 }
 
-func loadOpenAPISchema(oapiPath string) (*libopenapi.DocumentModel[v3.Document], error) {
+func loadOpenAPISchema(oapiPath string) (*libopenapi.DocumentModel[v3.Document], []error) {
 
 	oapiBytes, err := os.ReadFile(oapiPath)
 	if err != nil {
-		return nil, err
+		return nil, []error{err}
 	}
 
 	doc, err := libopenapi.NewDocument(oapiBytes)
 	if err != nil {
-		return nil, err
+		return nil, []error{err}
 	}
 
 	docValidator, validatorErrs := validator.NewValidator(doc)
 
 	if validatorErrs != nil {
-		return nil, validatorErrs[0]
+		return nil, validatorErrs
 	}
 
-	// 4. Validate!
 	valid, validationErrs := docValidator.ValidateDocument()
 
 	if !valid {
+		var errs []error
+
 		for _, e := range validationErrs {
-			// 5. Handle the error
-			fmt.Printf("Type: %s, Failure: %s\n", e.ValidationType, e.Message)
-			fmt.Printf("%d %d\n", e.SpecLine, e.SpecCol)
-			fmt.Printf("%v\n", e)
-			fmt.Printf("%v\n", e.SchemaValidationErrors)
-			fmt.Printf("Fix: %s\n\n", e.HowToFix)
+			for _, err := range e.SchemaValidationErrors {
+				errs = append(errs, err)
+			}
 		}
+
+		return nil, errs
 	}
 
 	v3Model, _ := doc.BuildV3Model()
-
 	return v3Model, nil
-}
-
-type Format struct {
-	base *excelize.File
-}
-
-func (f *Format) Set(sheet string, pos []interface{}, vals []interface{}) string {
-	// log.Println(f.base)
-	for idx, p := range pos {
-		err := f.base.SetCellValue(sheet, p.(string), vals[idx].(string))
-		if err != nil {
-			log.Println(err)
-		}
-	}
-	return "a"
 }
 
 func main() {
 
 	formatPath := flag.String("format", "", "output base file")
+	outputPath := flag.String("output", "out.xlsx", "output file")
 	oapiPath := flag.String("oapi", "spec.yaml", "openapi spec yaml file")
 	templPath := flag.String("templ", "template.js", "template file (javascript)")
 	flag.Parse()
 
-	baseFormat, err := loadFormatFile(*formatPath)
-	defer baseFormat.Close()
+	format, err := loadFormatFile(*formatPath)
+	defer format.Close()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	format := Format{base: baseFormat}
-	// log.Println(baseFormat)
 
-	oapi, err := loadOpenAPISchema(*oapiPath)
-	if err != nil {
-		log.Fatalln(err)
+	oapi, errs := loadOpenAPISchema(*oapiPath)
+	if errs != nil && len(errs) != 0 {
+		for _, err := range errs {
+			fmt.Println(err)
+		}
 	}
 
 	registry := new(require.Registry)
@@ -99,9 +84,12 @@ func main() {
 	registry.Enable(vm)
 	console.Enable(vm)
 
-	vm.Set("doc", oapi)
-	vm.Set("set", format.Set)
-	vm.Set("offsets", Offset)
+	vm.Set("OpenApiDocument", oapi)
+	vm.Set("OutputBook", format)
+
+	vm.Set("CellNameToCoordinates", excelize.CellNameToCoordinates)
+	vm.Set("CoordinatesToCellName", excelize.CoordinatesToCellName)
+	vm.Set("CellNameToCoordinates", excelize.CellNameToCoordinates)
 
 	templBytes, err := os.ReadFile(*templPath)
 	if err != nil {
@@ -113,31 +101,5 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	format.base.SaveAs("out.xlsx")
-}
-
-func Offset(coords []string, axis int, offset int) []string {
-	var result []string
-	for _, coord := range coords {
-		col, row, err := excelize.CellNameToCoordinates(coord)
-		if err != nil {
-			return nil
-		}
-
-		if axis == 0 {
-			coord, err := excelize.CoordinatesToCellName(col+offset, row)
-			if err != nil {
-				return nil
-			}
-			result = append(result, coord)
-		} else {
-			coord, err := excelize.CoordinatesToCellName(col, row+offset)
-			if err != nil {
-				return nil
-			}
-			result = append(result, coord)
-		}
-	}
-
-	return result
+	format.SaveAs(*outputPath)
 }
--- templ.js
+++ templ.js
@@ -1,9 +1,31 @@
 const methods = ["Get", "Post", "Delete", "Patch", "Put"]
 
+function sets(book, sheet, coords, vals) {
+  coords.forEach((c, idx) => {
+    const err = book.SetCellValue(sheet, c, vals[idx])
+    if (err) {
+      console.log(err)
+    }
+  })
+}
+
+function offsets(coords, axis, offset) {
+  return coords.map((c) => {
+    var err, col, row, coord
+		[col, row] = CellNameToCoordinates(c)
+
+    if (axis == 0) {
+			return CoordinatesToCellName(col+offset, row)
+    } else {
+			return CoordinatesToCellName(col, row+offset)
+    }
+  })
+}
+
 // output interface list
 // exampls:
-// | No. | IFName(ja) | IFName(en) | Dest. Service | Method |
-// | 1   | hogehoge | /hogehoge | Hoge service | GET |
+// | No. | IFName(ja) | IFName(en) | Dest. Service | Method | Description |
+// | 1   | hogehoge   | /hogehoge  | Hoge service  | GET    | hogehoge    |
 function makeInterfaceList(doc) {
   const paths = doc.Model.Paths.PathItems;
 
@@ -15,7 +37,7 @@ function makeInterfaceList(doc) {
         return
       }
   
-      vals = [idx, paths[pathname][method].Summary, pathname, "", method]
+      vals = [idx, paths[pathname][method].Summary, pathname, "", method, paths[pathname][method].Description]
       idx++
       output.push(vals)
     })
@@ -45,61 +67,41 @@ function makeInterfaceDescription(doc, pathname, method) {
   if (!operation.Responses.Codes[200].Content["application/json"]) return
 
   const schema = operation.Responses.Codes[200].Content["application/json"].Schema.Schema()
-
-  function inner(schema, index, name, indent, required) {
-    if (schema.Type == "object") {
-      var output = []
-      Object.keys(schema.Properties).forEach(function (propname, i) {
-        rows = inner(schema.Properties[propname].Schema(), index+i, propname, indent+1, schema.Required)
-        output = output.concat(rows)
-      })
-      return output
-    } else if (schema.Type == "array") {
-      var output = []
-      output.push([index, indent, name, schema.Type[0], true, required.includes(name), schema.Description])
-      var rows = inner(schema.Items.A.Schema(), index+1, "", indent, required)
-      output = output.concat(rows)
-      return output
-    } else {
-      return [[index, indent, name, schema.Type[0], false, required.includes(name), schema.Description]]
-    }
-  }
-
-  return inner(schema, 1, "", 0, [])
+  return schemaToRows(schema, 1, "", 0, [])
 }
 
-function main(doc) {
-  var pos = ["A2", "B2", "C2", "D2", "E2"]
+function schemaToRows(schema, index, name, indent, required) {
+  if (schema.Type == "object") {
+    var output = []
+    Object.keys(schema.Properties).forEach(function (propname, i) {
+      rows = schemaToRows(schema.Properties[propname].Schema(), index+i, propname, indent+1, schema.Required)
+      output = output.concat(rows)
+    })
+    return output
+  } else if (schema.Type == "array") {
+    var output = []
+    output.push([index, indent, name, schema.Type[0], true, required.includes(name), schema.Description])
+    var rows = schemaToRows(schema.Items.A.Schema(), index+1, "", indent, required)
+    output = output.concat(rows)
+    return output
+  } else {
+    return [[index, indent, name, schema.Type[0], false, required.includes(name), schema.Description]]
+  }
+}
+
+function main(doc, book) {
+  var pos = ["B4", "C4", "D4", "E4", "F4", "G4"]
   var offset = 1
 
   makeInterfaceList(doc).forEach((vals) => {
-    // set("Sheet", pos, vals)
-    // pos = offsets(pos, 1, 1)
-    console.log(vals)
+    sets(book, "List", pos, vals)
+    pos = offsets(pos, 1, 1)
+
     var output = makeInterfaceDescription(doc, vals[2], vals[4])
     console.log(JSON.stringify(output))
   })
 
 }
 
-
-
-main(doc)
-console.log("finished");
-// console.log(Object.keys(doc))
-// console.log(Object.keys(doc.Model))
-// console.log(Object.keys(doc.Model.Paths))
-// console.log(Object.keys(doc.Model.Paths.PathItems["/pets"]))
-// console.log(doc.Model.Paths.PathItems["/pets"].Get.Summary)
-// console.log(Object.keys(doc.Model.Paths.PathItems["/pets"].Get))
-// console.log(Object.keys(doc.Model.Paths.PathItems["/pets"].Get.Responses.Codes[200].Content["application/json"].Schema.BuildSchema().Properties))
-// console.log(Object.keys(doc.Model.Paths.PathItems["/pets"].Get.Responses.Codes[200].Content["application/json"].Schema.Schema()))
-// console.log(doc.Model.Paths.PathItems["/pets"].Get.Responses.Codes[200].Content["application/json"].Schema.Schema().Type)
-// console.log(doc.Model.Paths.PathItems["/pets"].Get.Responses.Codes[200].Content["application/json"].Schema.Schema().Items[0].Schema().Properties)
-// console.log(Object.keys(doc.Model.Paths.PathItems["/pets"].Get.Responses.Codes[200].Content["application/json"].Schema.Schema().Items.A.Schema().Properties))
-
-
-// var pos = ["B2", "E2", "I2"]
-// set("シート1", pos, ["1", "2", "33"])
-// set("Sheet1", pos, ["1", "2", "33"])
-//  .Model.Paths.PathItems.length);
+// below arguments pass by the golang host program
+main(OpenApiDocument, OutputBook)
