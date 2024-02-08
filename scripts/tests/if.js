cc7806fa0c94ea08939516258868e151afb9767b
yaamai <ya_amai@yahoo.co.jp>
2024-02-08 21:26:37 +0000

    add test

--- scripts/tests/if.js
+++ scripts/tests/if.js
@@ -2,6 +2,52 @@ const utils = require('./scripts/utils.js')
 const oapi = require('./scripts/oapi.js')
 
 TEST_DATA = yaml(`
+- desc: array and object deeply nested
+  expect:
+  - ["AAA", ["Hoge"], 0]
+  - ["CCC", ["Hoge", "aaa"], 1]
+  - ["DDD", ["Hoge", "bbb"], 1]
+  - ["EEE", ["Hoge", "bbb", "Bar"], 2]
+  - ["FFF", ["Hoge", "bbb", "Bar", "list"], 3]
+  - ["GGG", ["Hoge", "bbb", "Bar", "list", "Baz"], 4]
+  name: Hoge
+  input: |
+    openapi: 3.0.1
+    info:
+      title: api
+      version: 1.0.0
+    paths: {}
+    components:
+      schemas:
+        Hoge:
+          allOf:
+          - $ref: "#/components/schemas/Foo"
+          - x-janame: AAA
+        Foo:
+          type: object
+          description: BBB
+          properties:
+            aaa:
+              description: CCC
+              type: string
+            bbb:
+              type: array
+              description: DDD
+              items:
+                $ref: "#/components/schemas/Bar"
+        Bar:
+          type: object
+          description: EEE
+          properties:
+            list:
+              type: array
+              description: FFF
+              items:
+                $ref: "#/components/schemas/Baz"
+        Baz:
+          type: object
+          description: GGG
+
 - desc: altname of object and description allOf
   expect:
   - ["AAA", ["Hoge"], 0]
