f76f037dcb02f0738b0dd353e22896dbed0a62d8
yaamai <ya_amai@yahoo.co.jp>
2024-03-18 13:00:15 +0900

    impl new db generator

--- scripts/bin/db.js
+++ scripts/bin/db.js
@@ -1,13 +1,41 @@
 const utils = require('./scripts/utils.js')
 const db = require('./scripts/db.js')
 const excel = require('./scripts/excel.js')
+const oapi = require('./scripts/oapi.js')
 
 function main() {
   var data = file(args()[0])
   var [doc, err] = openapischema(data)
   utils.assert(err, [])
 
-  dummy(doc)
+  var book = excelfile(args()[1])
+
+  // dummy(doc)
+  for(let [method, pathname, oper] of oapi.iterOperations(doc)) {
+    const [pathParameter, reqSchema, respSchema] = oapi.getOperationSchemas(oper)
+
+    let converter = null
+
+    if (reqSchema) {
+      const reqSchemaName = oapi.getRefName(reqSchema)
+      console.log(reqSchemaName)
+      converter = new db.OAPIToDBConverter(reqSchemaName, reqSchema)
+      converter.process()
+      console.log(JSON.stringify(converter.result, null, "  "))
+      tablesToExcel(book, converter.result)
+    }
+
+    if (respSchema) {
+      const respSchemaName = oapi.getRefName(respSchema)
+      console.log(respSchemaName)
+      converter = new db.OAPIToDBConverter(respSchemaName, respSchema)
+      converter.process()
+      console.log(JSON.stringify(converter.result, null, "  "))
+      tablesToExcel(book, converter.result)
+    }
+  }
+
+  book.SaveAs(args()[2])
 }
 
 function dummy(doc) {
@@ -74,9 +102,12 @@ function tablesToExcel(book, tables) {
 
     let pos = ["F8", "Q8", "AB8", "AK8", "AU8"]
     for(let colName of Object.keys(tables[tableName].columns)) {
-      const col = tables[tableName][colName]
+      console.log(colName)
+      const col = tables[tableName]["columns"][colName]
       excel.sets(book, sheetName, pos, [col.altname, col.name, col.type, "", col.desc])
       pos = excel.offsets(pos, 1, 1)
     }
-  })
+  }
 }
+
+main()
